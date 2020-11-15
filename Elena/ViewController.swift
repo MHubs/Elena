ec49ab6b98f8702886f21151b07d095befc72b4f
Maxwell Hubbard <hubs.maxwell@gmail.com>
2020-11-15 13:44:47 -0500

    - Added a forgotten error handling thing - Switched to use walking graph

--- Elena/ViewController.swift
+++ Elena/ViewController.swift
@@ -361,14 +361,14 @@ class ViewController: UIViewController, UITextFieldDelegate, CLLocationManagerDe
         let requestURL = "http://165.227.197.221:5000/route"
         
         let Url = String(format: requestURL)
-        guard let serviceUrl = URL(string: Url) else { return }
+        guard let serviceUrl = URL(string: Url) else {return }
         let parameters: [String: Any] = [
             "start": starting,
             "dest": ending,
             "goal": Settings.instance.elevationGain,
             "limit": String(Settings.instance.tolerance),
             "algorithm": "AStar",
-            "method": "drive"
+            "method": "walk"
         ]
         var request = URLRequest(url: serviceUrl)
         request.httpMethod = "POST"
@@ -395,11 +395,15 @@ class ViewController: UIViewController, UITextFieldDelegate, CLLocationManagerDe
                 } catch {
                     
                     // Error
-                    
                     print(error)
                     DispatchQueue.main.async {
                         self.displayError(error.localizedDescription)
                     }
+                }
+            } else {
+                print(error)
+                DispatchQueue.main.async {
+                    self.displayError(error!.localizedDescription)
                 }
             }
         }.resume()
